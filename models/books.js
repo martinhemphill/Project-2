89cea2779a7a683f1dca5d290b41c05b8eb09178
Garrett Hudson <garretthudson87@yahoo.com>
2020-10-03 10:00:10 -0400

    committing just to pull

--- models/books.js
+++ models/books.js
@@ -12,7 +12,7 @@ module.exports = (sequelize, DataTypes) => {
       allowNull: false
     },
     title: {
-      type: DataTypes.STRING,
+      type: DataTypes.TEXT,
       allowNull: false
     },
     description: {
